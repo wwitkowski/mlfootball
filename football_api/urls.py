4df346b67cf2c182fb837f99c7da8112496902c2
wwitkowski <wojciechwitkowski94@gmail.com>
2021-07-16 14:18:05 +0200

    connect to database

--- football_api/urls.py
+++ football_api/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mlfootball.urls', namespace='mlfootball')),
-    path('/api', include('mlfootball_api.urls', namespace='mlfootball_api'))
+    path('api/', include('mlfootball_api.urls', namespace='mlfootball_api'))
 ]
