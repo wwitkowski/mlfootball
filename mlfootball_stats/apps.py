a6b0545fa83cfc15755dd17c95ce099bb9c41fc0
wwitkowski <wojciechwitkowski94@gmail.com>
2021-12-04 13:06:25 +0100

    delete tempaltes

--- mlfootball_stats/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MlfootballConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mlfootball_stats'
--- mlfootball_stats/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_view, match_stats_view
-
-app_name = 'mlfootball_stats'
-
-urlpatterns = [
-    path('', home_view),
-    path('<int:match_id>/', match_stats_view)
-]
